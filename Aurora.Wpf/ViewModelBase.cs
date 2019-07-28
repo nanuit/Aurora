2d63ccb2da51b5be09760a0b36bc87a4c6297fec
nanuit <nanuit@ossi.at>
2019-07-28 07:10:25 +0200

    ViewModelBase added

---
+++ Aurora.Wpf/ViewModelBase.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aurora.Wpf
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        #region INotifyPropertyChange
+        public event PropertyChangedEventHandler PropertyChanged;
+        
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+
+
+
+    }
+}
