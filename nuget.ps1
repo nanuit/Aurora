0d34628337729101492e33139367a9f2061dab59
Christian Oswald <christian.oswald@unicredit.at>
2019-10-01 11:49:09 +0200

    added nuget packagecreation

---
+++ nuget.ps1
@@ -0,0 +1,5 @@
+nuget pack Aurora.Configs\Aurora.Configs.csproj -OutputDirectory ./NuPkg -Properties Configuration=Release
+nuget pack Aurora.Crypt\Aurora.Crypt.csproj -OutputDirectory ./NuPkg -Properties Configuration=Release
+nuget pack Aurora.Crypt.Forms\Aurora.Crypt.Forms.csproj -OutputDirectory ./NuPkg -Properties Configuration=Release
+nuget pack Aurora.IO\Aurora.IO.csproj -OutputDirectory ./NuPkg -Properties Configuration=Release
+nuget pack Aurora.Wpf\Aurora.Wpf.csproj -OutputDirectory ./NuPkg -Properties Configuration=Release
