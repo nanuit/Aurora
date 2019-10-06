1e91bae39073d3f48c52f93c261b4c04f6f720ea
nanuit <nanuit@ossi.at>
2019-10-06 12:27:41 +0200

    Math and named pipe classes added

---
+++ Aurora.Math/Math.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helper.Misc
+{
+    public class Math
+    {
+        public static int GetPercentage(int value, int percent)
+        {
+            return value > 0 ? (System.Math.Min(Convert.ToInt32(System.Math.Ceiling(percent / ((double)value / 100))), 100)) : (0);
+        }
+    }
+}
--- nuget.ps1
+++ nuget.ps1
@@ -3,3 +3,4 @@ nuget pack Aurora.Crypt\Aurora.Crypt.csproj -OutputDirectory ./NuPkg -Properties
 nuget pack Aurora.Crypt.Forms\Aurora.Crypt.Forms.csproj -OutputDirectory ./NuPkg -Properties Configuration=Release
 nuget pack Aurora.IO\Aurora.IO.csproj -OutputDirectory ./NuPkg -Properties Configuration=Release
 nuget pack Aurora.Wpf\Aurora.Wpf.csproj -OutputDirectory ./NuPkg -Properties Configuration=Release
+nuget pack Aurora.Math\Aurora.Math.csproj -OutputDirectory ./NuPkg -Properties Configuration=Release
