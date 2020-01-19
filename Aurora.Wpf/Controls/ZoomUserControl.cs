b62cd5a284cb5dcba57bd5413e048f60e9e7586d
nanuit <nanuit@ossi.at>
2020-01-19 22:11:16 +0100

    Zoom for UserControl added

---
+++ Aurora.Wpf/Controls/ZoomUserControl.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+
+namespace Aurora.Wpf.Controls
+{
+    public class ZoomUserControl : UserControl
+    {
+        protected ZoomUserControl()
+        {
+            PreviewMouseWheel += OnMouseWheel;
+        }
+        private float m_ScaleFactor = 1.0f;
+        private void OnMouseWheel(object sender, MouseWheelEventArgs e)
+        {
+
+            if (Keyboard.Modifiers != ModifierKeys.Control)
+                return;
+
+            if (!(this.LayoutTransform is ScaleTransform scaler))
+            {
+                scaler = new ScaleTransform(1.0, 1.0);
+                this.LayoutTransform = scaler;
+            }
+
+            // We'll need a DoubleAnimation object to drive
+            // the ScaleX and ScaleY properties.
+
+            DoubleAnimation animator = new DoubleAnimation()
+            {
+                Duration = new Duration(TimeSpan.FromMilliseconds(600)),
+            };
+            if (e.Delta > 0)
+                m_ScaleFactor += 0.1f;
+            else
+                m_ScaleFactor -= 0.1f;
+
+            animator.To = m_ScaleFactor;
+            scaler.BeginAnimation(ScaleTransform.ScaleXProperty, animator);
+            scaler.BeginAnimation(ScaleTransform.ScaleYProperty, animator);
+
+        }
+    }
+}
