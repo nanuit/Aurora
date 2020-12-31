2ad8a5121ab85b5c39536e6ea243dd94f95eb172
ossi <ossi@ossi.at>
2020-12-31 10:57:06 +0100

    GetLinkerTimestamp added

--- Aurora.Misc/Application/ApplicationInfo.cs
+++ Aurora.Misc/Application/ApplicationInfo.cs
@@ -1,11 +1,11 @@
 ﻿using System;
+using System.IO;
 using System.Reflection;
 
 namespace Aurora.Misc.Application
 {
     public static class ApplicationInfo
     {
-        ///TODO: Move to Aurora
         /// <summary>
         /// Retrieves the linker timestamp.
         /// </summary>
@@ -25,6 +25,30 @@ namespace Aurora.Misc.Application
             return dt.AddHours(System.TimeZone.CurrentTimeZone.GetUtcOffset(dt).Hours);
         }
         /// <summary>
+        /// Retrieves the linker timestamp.
+        /// </summary>
+        /// <param name="filePath"></param>
+        /// <returns></returns>
+        /// <remarks>https://www.meziantou.net/getting-the-date-of-build-of-a-dotnet-assembly-at-runtime.htm</remarks>
+        public static DateTime GetLinkerTimestamp(string filePath)
+        {
+            const int peHeaderOffset = 60;
+            const int linkerTimestampOffset = 8;
+            var bytes = new byte[2048];
+
+            using (var file = new FileStream(filePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+            {
+                file.Read(bytes, 0, bytes.Length);
+            }
+
+            var headerPos = BitConverter.ToInt32(bytes, peHeaderOffset);
+            var secondsSince1970 = BitConverter.ToInt32(bytes, headerPos + linkerTimestampOffset);
+            var dt = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Local);
+            dt = dt.AddSeconds(secondsSince1970);
+            dt = dt.AddHours(System.TimeZone.CurrentTimeZone.GetUtcOffset(dt).Hours);
+            return dt;
+        }
+        /// <summary>
         /// get the linked timestamp for the assembly
         /// </summary>
         /// <param name="assembly">assembly to get the date for</param>
@@ -33,7 +57,7 @@ namespace Aurora.Misc.Application
         {
             //string path = Path.GetDirectoryName(new Uri(assembly.GetName().CodeBase).LocalPath);
             string path = new Uri(assembly.GetName().CodeBase).LocalPath;
-            return (RetrieveLinkerTimestamp(path));
+            return (GetLinkerTimestamp(path));
         }
         /// <summary>
         /// Retreive the version 
