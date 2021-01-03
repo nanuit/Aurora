c6ecf351aa9aa9397b9f5c62feadcb32bbf09fd8
ossi <ossi@ossi.at>
2021-01-03 10:52:55 +0100

    added Unit tests for wpf and Misc

---
+++ UnitTest/AuroraWpf.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestLib
+{
+    [TestClass]
+    public class AuroraWpf
+    {
+        [TestMethod]
+        public void TestMainWindow()
+        {
+
+
+        }
+    }
+}
