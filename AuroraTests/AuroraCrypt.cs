c8279bb0e4449cc31b3edd552e9d2ea57620c82d
ossi <ossi@ossi.at>
2022-10-01 23:40:48 +0200

    reworked Encryption

--- AuroraTests/AuroraCrypt.cs
+++ AuroraTests/AuroraCrypt.cs
@@ -5,7 +5,7 @@ namespace AuroraUnitTests
     public class AuroraCrypt
     {
         [Test]
-        public void TestEncrypt()
+        public void TestDeEncrypt()
         {
             string originalString = "dummygummi20";
             string key = "myapp";
@@ -13,8 +13,15 @@ namespace AuroraUnitTests
             Console.WriteLine($"Original:{originalString} key:{key} Encoded String:{encoded}");
 
             string decoded = Encryption.Decrypt(encoded, key);
+            Console.WriteLine($"Encoded:{encoded} key:{key} Decoded String:{decoded}");
+        }
+        [Test]
+        public void TestDecrypt()
+        {
+            string key= "B2C1323D-038B-4E8A-A142-BE8C20009A0D";
+            string encoded = "h6vzidqjL10nc0HnLFhlYxhbvnFLmG5rK4FrGm4MLe78T+YVoqQDYCYnRbxhI5/RH5TODbNSquF++s/ku/UdHg==";
+            string decoded = Encryption.Decrypt(encoded, key);
             Console.WriteLine($"Encoded:{encoded} key:{key} Deccoded String:{decoded}");
-
         }
     }
 }
