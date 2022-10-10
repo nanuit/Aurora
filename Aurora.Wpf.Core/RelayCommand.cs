ba7be7f8ab8d361e550be66bebc5b15da14dc700
ossi <ossi@ossi.at>
2022-10-10 22:38:50 +0200

    Code cleanup

--- Aurora.Wpf.Core/RelayCommand.cs
+++ Aurora.Wpf.Core/RelayCommand.cs
@@ -6,29 +6,32 @@ namespace Aurora.Wpf.Core
 {
     public class RelayCommand : ICommand
     {
-        #region Fields 
-        readonly Action<object> m_Execute;
-        readonly Predicate<object> m_CanExecute;
+        #region Fields
+        private readonly Action<object>? m_Execute;
+        private readonly Predicate<object>? m_CanExecute;
         #endregion // Fields 
-        #region Constructors 
-        public RelayCommand(Action<object> execute) : this(execute, null) { }
-        public RelayCommand(Action<object> execute, Predicate<object> canExecute)
+        #region Constructors
+
+        public RelayCommand(Action<object> execute, Predicate<object>? canExecute = null)
         {
-            m_Execute = execute ?? throw new ArgumentNullException("execute"); m_CanExecute = canExecute;
+            m_Execute = execute ?? throw new ArgumentNullException(nameof(execute)); m_CanExecute = canExecute;
         }
         #endregion // Constructors 
         #region ICommand Members 
         [DebuggerStepThrough]
-        public bool CanExecute(object parameter)
+        public bool CanExecute(object? parameter)
         {
-            return m_CanExecute?.Invoke(parameter) ?? true;
+            return parameter != null && (m_CanExecute?.Invoke(parameter) ?? true);
         }
-        public event EventHandler CanExecuteChanged
+        public event EventHandler? CanExecuteChanged
         {
             add => CommandManager.RequerySuggested += value;
             remove => CommandManager.RequerySuggested -= value;
         }
-        public void Execute(object parameter) { m_Execute(parameter); }
+        public void Execute(object? parameter)
+        {
+            if (parameter != null) m_Execute?.Invoke(parameter);
+        }
         #endregion 
     }
 }
