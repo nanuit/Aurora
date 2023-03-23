7de10d1fbfc3a830ced4b7b060163328c8243db6
ossi <ossi@ossi.at>
2023-03-23 13:07:59 +0100

    error in EnsureDirectory fixed

---
+++ AuroraTests/AuroraIO.cs
@@ -0,0 +1,20 @@
+﻿using Aurora.IO;
+
+namespace AuroraUnitTests
+{
+    internal class AuroraIO
+    {
+        [Test]
+        public void TestEnsureDirectory()
+        {
+            try
+            {
+                Aurora.IO.Directory.EnsureDirectory(@"c:\var\gummigut");
+                Aurora.IO.Directory.EnsureFileDirectory(@"c:\var\gummigut\filedir\file.txt");
+                Aurora.IO.Directory.EnsureSubDirectory(@"c:\var\gummigut", "subdir");
+
+            }
+            catch (Exception ex) { Console.WriteLine(ex.Message); }
+        }
+    }
+}
--- AuroraTests/AuroraMisc.cs
+++ AuroraTests/AuroraMisc.cs
@@ -29,4 +29,5 @@ public class AuroraMisc
         }
         catch (Exception ex) { Console.WriteLine(ex.Message); }
     }
+
 }
